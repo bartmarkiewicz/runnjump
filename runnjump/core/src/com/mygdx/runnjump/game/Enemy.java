1d0500e33c6c41af8692110e0e1ffd5fd114c923
Bartosz Markiewicz <bmar0027@student.monash.edu>
2021-01-17 23:32:05 +0000

    Minor addition to enemy class.

--- runnjump/core/src/com/mygdx/runnjump/game/Enemy.java
+++ runnjump/core/src/com/mygdx/runnjump/game/Enemy.java
@@ -2,59 +2,41 @@ package com.mygdx.runnjump.game;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.math.Vector2;
 
 
 /**
  * A class used to represent an enemy NPC and its assorted AI and the logic related to it. Unfinished as of week 11.
  */
-public class Enemy {
-    private Vector2 velocity = new Vector2();
-    private float speedX = 200;
-    private float speedY = 0;
-    private float gravity = 140f;
-    private boolean gravityPowerUp;
-    /**
-     * The Can jump.
-     */
-    boolean canJump;
-    /**
-     * The Size x.
-     */
-    float sizeX,
-    /**
-     * The Size y.
-     */
-    sizeY;
-    /**
-     * The Enemy sprite.
-     */
-    Sprite enemySprite;
-    private boolean goingBack = false;
+public class Enemy extends MovingActor{
 
+    public Enemy(TiledMapTileLayer collisionLayer, TiledMapTileLayer visualLayer) {
+        super(collisionLayer, visualLayer);
+    }
 
-    /**
-     * Instantiates a new Enemy.
-     */
-    public Enemy(){
+    @Override
+    protected void handleMisc(float x, float y) {
 
     }
 
-    /**
-     * Draws the enemy sprite using the batch.
-     *
-     * @param batch the batch
-     */
-    public void draw(Batch batch){
-        enemySprite.draw(batch);
+    @Override
+    protected void handleCollectible(float x, float y) {
+
     }
 
-    /**
-     * Updates the enemy.
-     *
-     * @param delta the time since last call of render method
-     */
-    public void update(float delta){
+    @Override
+    protected void update(float delta) {
 
+    }
+
+    @Override
+    protected void die() {
+
+    }
+
+    @Override
+    public boolean respawn() {
+        return false;
     }
 }
