8ee2d360e75501e81eea166e8533dab0eff51945
bartmarkiewicz <bartmark322@gmail.com>
2021-02-01 11:40:24 +0000

    Some level work done

---
+++ runnjump/android/assets/levels/collectAndEnvJungle.tsx
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="collectAndEnvJungle" tilewidth="32" tileheight="32" tilecount="288" columns="12">
+ <image source="collectAndEnvJungle.png" width="384" height="774"/>
+ <tile id="0">
+  <properties>
+   <property name="gold_key" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="gold_key" value=""/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="coin" value=""/>
+  </properties>
+  <animation>
+   <frame tileid="60" duration="100"/>
+   <frame tileid="62" duration="100"/>
+   <frame tileid="84" duration="100"/>
+   <frame tileid="86" duration="100"/>
+   <frame tileid="108" duration="100"/>
+   <frame tileid="110" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="coin" value=""/>
+  </properties>
+  <animation>
+   <frame tileid="61" duration="100"/>
+   <frame tileid="63" duration="100"/>
+   <frame tileid="85" duration="100"/>
+   <frame tileid="87" duration="100"/>
+   <frame tileid="109" duration="100"/>
+   <frame tileid="111" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="72">
+  <properties>
+   <property name="coin" value=""/>
+  </properties>
+  <animation>
+   <frame tileid="72" duration="100"/>
+   <frame tileid="74" duration="100"/>
+   <frame tileid="96" duration="100"/>
+   <frame tileid="98" duration="100"/>
+   <frame tileid="120" duration="100"/>
+   <frame tileid="122" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="73">
+  <properties>
+   <property name="coin" value=""/>
+  </properties>
+  <animation>
+   <frame tileid="73" duration="100"/>
+   <frame tileid="75" duration="100"/>
+   <frame tileid="97" duration="100"/>
+   <frame tileid="99" duration="100"/>
+   <frame tileid="121" duration="100"/>
+   <frame tileid="123" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="76">
+  <properties>
+   <property name="gravity_powerup" value=""/>
+  </properties>
+ </tile>
+ <tile id="77">
+  <properties>
+   <property name="gravity_powerup" value=""/>
+  </properties>
+ </tile>
+ <tile id="78">
+  <properties>
+   <property name="gravity_powerup" value=""/>
+  </properties>
+ </tile>
+ <tile id="79">
+  <properties>
+   <property name="gravity_powerup" value=""/>
+  </properties>
+ </tile>
+ <tile id="80">
+  <properties>
+   <property name="win_flag" value=""/>
+  </properties>
+ </tile>
+ <tile id="88">
+  <properties>
+   <property name="gravity_powerup" value=""/>
+  </properties>
+ </tile>
+ <tile id="89">
+  <properties>
+   <property name="gravity_powerup" value=""/>
+  </properties>
+ </tile>
+ <tile id="90">
+  <properties>
+   <property name="gravity_powerup" value=""/>
+  </properties>
+ </tile>
+ <tile id="91">
+  <properties>
+   <property name="gravity_powerup" value=""/>
+  </properties>
+ </tile>
+ <tile id="92">
+  <properties>
+   <property name="win_flag" value=""/>
+  </properties>
+ </tile>
+ <tile id="148">
+  <properties>
+   <property name="checkpoint_flag" value=""/>
+  </properties>
+ </tile>
+ <tile id="160">
+  <properties>
+   <property name="checkpoint_flag" value=""/>
+  </properties>
+ </tile>
+ <tile id="180">
+  <properties>
+   <property name="heart" value=""/>
+  </properties>
+ </tile>
+ <tile id="181">
+  <properties>
+   <property name="heart" value=""/>
+  </properties>
+ </tile>
+ <tile id="192">
+  <properties>
+   <property name="heart" value=""/>
+  </properties>
+ </tile>
+ <tile id="193">
+  <properties>
+   <property name="heart" value=""/>
+  </properties>
+ </tile>
+ <tile id="228">
+  <properties>
+   <property name="star" value=""/>
+  </properties>
+ </tile>
+ <tile id="229">
+  <properties>
+   <property name="star" value=""/>
+  </properties>
+ </tile>
+ <tile id="240">
+  <properties>
+   <property name="star" value=""/>
+  </properties>
+ </tile>
+ <tile id="241">
+  <properties>
+   <property name="star" value=""/>
+  </properties>
+ </tile>
+</tileset>
---
+++ runnjump/android/assets/levels/menubg.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="menubg" tilewidth="32" tileheight="32" tilecount="943" columns="41">
+ <image source="menubg.png" width="1334" height="750"/>
+</tileset>
