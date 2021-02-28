713ecf7bb57833198c87caec2bb6d481f808b247
bartmarkiewicz <bartmark322@gmail.com>
2021-02-28 23:29:22 +0000

    Changed PC default resolution.

--- runnjump/desktop/src/com/mygdx/runnjump/desktop/DesktopLauncher.java
+++ runnjump/desktop/src/com/mygdx/runnjump/desktop/DesktopLauncher.java
@@ -4,9 +4,16 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.runnjump.Runnjump;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Run N Jump";
+		config.forceExit = false;
+		config.height = 900;
+		config.width = 1600;
 		new LwjglApplication(new Runnjump(), config);
 
 	}
